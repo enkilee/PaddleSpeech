549fded429dbe35947dfb7bb93faf766bfa42263
enkilee <jeffrey0122@163.com>
2024-12-23 15:30:44 +0800

    fix

--- examples/csmsc/voc1/local/synthesize_e2e.sh
+++ examples/csmsc/voc1/local/synthesize_e2e.sh
@@ -2,14 +2,14 @@
 
 config_path=$1
 train_output_path=$2
-
+ckpt_name=$3
 
 FLAGS_allocator_strategy=naive_best_fit \
 FLAGS_fraction_of_gpu_memory_to_use=0.01 \
 python3 ${BIN_DIR}/../../synthesize_e2e.py \
     --am=fastspeech2_csmsc \
     --am_config=${config_path} \
-    --am_ckpt=fastspeech2_nosil_baker_ckpt_0.4/snapshot_iter_76000.pdz \
+    --am_ckpt=${train_output_path}/checkpoints/${ckpt_name} \
     --am_stat=dump/train/speech_stats.npy \
     --voc=pwgan_csmsc \
     --voc_config=pwg_baker_ckpt_0.4/pwg_default.yaml \
@@ -18,5 +18,5 @@ python3 ${BIN_DIR}/../../synthesize_e2e.py \
     --lang=zh \
     --text=${BIN_DIR}/../../assets/sentences.txt \
     --output_dir=${train_output_path}/test_e2e \
-    --phones_dict=fastspeech2_nosil_baker_ckpt_0.4/phone_id_map.txt \
+    --phones_dict=dump/phone_id_map.txt \
     --inference_dir=${train_output_path}/inference
