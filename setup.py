29216b6f60a10e7ade21fc739856aaf6db40574f
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2025-01-16 16:54:03 +0800

    Update setup.py (#3964)

--- setup.py
+++ setup.py
@@ -318,9 +318,9 @@ setup_info = dict(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     entry_points={
         'console_scripts': [
